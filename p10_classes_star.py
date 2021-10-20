02e8b2d84e47ed0079acfcf06ded3c6045dfc8f7
Maarten Dirickx <maartendirickx@gmail.com>
2021-10-20 14:20:19 +0200

    Add docstrings for dunder methods

--- p10_classes_star.py
+++ p10_classes_star.py
@@ -16,18 +16,27 @@ class star():
        returns the size through the telescope
     """
     def __init__(self, name, distance, size):
+        """Initialize self by name, distance and size
+
+        keyword attributes:
+        name -- (string) numerical name of star 
+        distance -- (float) distance from telescope
+        size -- (float) star size
+        """
         self.my_name = name
         self.my_distance = distance
         self.my_size = size
     
     def __str__(self):
+        """override string representation"""
         return f'Awesome star named {self.my_name} with a size of {self.my_size} at a distance of {self.my_distance}'
     
     def __repr__(self):
+        """override repr representation"""
         return f"{self.__class__.__name__}('{self.my_name}', {self.my_distance}, {self.my_size})"
     
     def get_visual_size(self):
-        "returns size as seen through the telescope"
+        """returns size as seen through the telescope"""
         print(self.my_name)
         print(self.my_distance)
         print(self.my_size)
